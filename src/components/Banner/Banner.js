671d04bc7bae52dcacb2e3109e5689bd588248f4
Elvio Rodriguez <elvio.martin@gmail.com>
2023-03-28 19:41:56 -0300

    Primeraentrega-ElvioRodriguez

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <div className='banner-container'>
+        <h1>Tienda de Zapatillas</h1>
+        </div>
+    </div>
+  )
+};
+
+export default Banner;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+ <div className='nav-container'>
+    <nav className='navbar'>
+       <h1 className='navbar-logo'>Tienda</h1>
+       <h2 className='carrito'>🛒</h2>
+    </nav>
+ </div>
+    
+  );
+};
+
+export default Navbar;
